2c3d94a4a65245c95fd60de89acd39150e343562
JCPosso <juancamiloposso01@gmail.com>
2025-10-28 21:35:21 -0500

    feat: Implement backend service with replicated map and REST controller

---
+++ src/main/java/co/edu/escuelaing/distributedsystem/backedservice/ReplicatedMapService.java
@@ -0,0 +1,31 @@
+
+import org.jgroups.*;
+import org.jgroups.blocks.ReplicatedHashMap;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+
+@Service
+public class ReplicatedMapService {
+    private JChannel channel;
+    private ReplicatedHashMap<String, String> map;
+
+    @PostConstruct
+    public void init() throws Exception {
+        channel = new JChannel();
+        map = new ReplicatedHashMap<>(channel);
+        channel.connect("DistributedCluster");
+        map.start(10000);
+        System.out.println("Joined cluster with current data: " + map);
+    }
+
+    public void put(String key, String value) {
+        map.put(key, value);
+        System.out.println("Added: " + key + " = " + value);
+    }
+
+    public Map<String, String> getAll() {
+        return map;
+    }
+}
---
+++ src/main/java/co/edu/escuelaing/distributedsystem/backedservice/controller/BackendController.java
@@ -0,0 +1,28 @@
+package co.edu.escuelaing.distributedsystem.backedservice.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.Map;
+
+@RestController
+public class BackendController {
+
+    @Autowired
+    private ReplicatedMapService replicatedMapService;
+
+    @PostMapping("/store")
+    public String store(@RequestBody Map<String, String> body) {
+        String name = body.get("name");
+        String timestamp = Instant.now().toString();
+        replicatedMapService.put(name, timestamp);
+        return "Stored " + name + " at " + timestamp;
+    }
+
+    @GetMapping("/state")
+    public Map<String, String> state() {
+        return replicatedMapService.getAll();
+    }
+}
